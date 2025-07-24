bb6d68257063ab055c70f0acab31e18a50fa4831
Dev Sharma <devsharma1683@gmail.com>
2025-07-24 20:36:16 +0530

    bug fixed and remove un-used import statement

--- src/components/CopyRightSection.jsx
+++ src/components/CopyRightSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CopyRightSection() {
   return (
     <div className='text-center p-5 text-gl text-gray-300 bg-black hover:text-white transition'>
-        <span><i class="ri-copyright-line"></i></span> {new Date().getFullYear()} Raj Shamani. All rights reserved.
+        <span><i className="ri-copyright-line"></i></span> {new Date().getFullYear()} Raj Shamani. All rights reserved.
     </div>
   )
 }
--- src/components/reactBits/GooeyNav.jsx
+++ src/components/reactBits/GooeyNav.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const GooeyNav = ({
   items,
